368ab50c4b5a777a43c91c76cc33dfb7793fd856
Paulo <commonzenpython@gmail.com>
2015-10-03 13:02:25 -0400

    make sure sso is set

--- aldryn_sso/urls.py
+++ aldryn_sso/urls.py
@@ -6,14 +6,12 @@ from .sso import CloudUserClient
 from .views import CreateUserView, login_as_user
 
 
-client = CloudUserClient.from_dsn(settings.SSO_DSN)
-
-
 if getattr(settings, 'LOCAL_DEVELOPMENT', False):
     urlpatterns = patterns(
         '',
         url(r'^create-user/$', CreateUserView.as_view(), name='aldryn_local_create_user'),
         url(r'^login/$', login_as_user, name='aldryn_local_login'),
     )
-else:
+elif getattr(settings, 'SSO_DSN', False):
+    client = CloudUserClient.from_dsn(settings.SSO_DSN)
     urlpatterns = patterns('', url(r'^login/', include(client.get_urls())))
