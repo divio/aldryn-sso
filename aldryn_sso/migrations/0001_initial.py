814ab4d10e3f24703f3641a173dd3c3f209bbab0
Martin Koistinen <mkoistinen@gmail.com>
2015-11-16 19:58:08 -0500

    Add Django migrations

---
+++ aldryn_sso/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='AldrynCloudUser',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('cloud_id', models.PositiveIntegerField(unique=True)),
+                ('user', models.OneToOneField(related_name='aldryn_cloud_account', to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
