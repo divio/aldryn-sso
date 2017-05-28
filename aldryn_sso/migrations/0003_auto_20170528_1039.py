0b62c4d17c77e9c00d4c7da1d2725df6d0ec7163
Stefan Foulis <stefan@foulis.ch>
2017-05-28 10:40:02 +0200

    Add missing migrations for minor model changes
    No actual database level changes were missing in the migration. It is just to make django stop complaining about missing migrations.

---
+++ aldryn_sso/migrations/0003_auto_20170528_1039.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('aldryn_sso', '0002_auto_20161019_1953'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='aldrynclouduser',
+            name='cloud_id',
+            field=models.PositiveIntegerField(unique=True, verbose_name='Cloud ID'),
+        ),
+        migrations.AlterField(
+            model_name='aldrynclouduser',
+            name='user',
+            field=models.OneToOneField(related_name='aldryn_cloud_account', verbose_name='User', to=settings.AUTH_USER_MODEL),
+        ),
+    ]
