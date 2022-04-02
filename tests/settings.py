2f264ebe4ebad39a4bcad5becdff7687f6eb5d0a
Angelo Dini <angelo.dini@divio.ch>
2022-04-02 13:56:48 +0200

    we still need CMS

--- tests/settings.py
+++ tests/settings.py
@@ -3,12 +3,19 @@ HELPER_SETTINGS = {
     'INSTALLED_APPS': [],
     'SECRET_KEY': 'some-secret',
     'ALLOWED_HOSTS': ['localhost'],
+    'CMS_LANGUAGES': {
+        1: [{
+            'code': 'en',
+            'name': 'English',
+        }]
+    },
+    'LANGUAGE_CODE': 'en',
 }
 
 
 def run():
     from app_helper import runner
-    runner.run('aldryn_sso')
+    runner.cms('aldryn_sso')
 
 
 if __name__ == '__main__':
